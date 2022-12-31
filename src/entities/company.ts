03a8b1145013651cf03408aac339ce8b476747cf
VOncleyton Martins <martins.voncleyton@gmail.com>
2022-12-31 15:55:12 -0300

    feature: ensure that the app throw an error when no parking lots of any type are provided

--- src/entities/company.ts
+++ src/entities/company.ts
@@ -16,6 +16,8 @@ export class Company {
   constructor(props: CompanyProps, id?: string) {
     if (!this.validateCNPJ(props.CNPJ)) throw new Error('Invalid CNPJ');
 
+    if (props.motorcycleLots <= 0 && props.carLots <=0) throw new Error('You must inform at least one lot for motorcycles or cars');
+
     this.props = {
       ...props,
       _id: props._id ?? crypto.randomUUID()
