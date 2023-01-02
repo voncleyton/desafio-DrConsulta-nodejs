3a7052742f712b5dabf057523591216ba9add1b9
VOncleyton Martins <martins.voncleyton@gmail.com>
2023-01-02 20:00:37 -0300

    feature: ensure that the app throws when a invalid plate is provided

--- src/entities/vehicle.ts
+++ src/entities/vehicle.ts
@@ -13,10 +13,18 @@ export class Vehicle {
   private props: VehicleProps;
 
   constructor(props: VehicleProps, id?: string) {
+    if (!this.validatePlate(props.plate)) throw new Error('Invalid plate');
+
     this.props = {
       ...props,
       _id: props._id ?? crypto.randomUUID()
     }
+  }
+
+  private validatePlate(plate: string): boolean | null {
+    const validRegExpPlate = '[A-Z]{3}[0-9][0-9A-Z][0-9]{2}';
+    const isValidPlate = !!plate.match(validRegExpPlate);
+    return isValidPlate;
   }
 
   public get _id (): string | undefined {
