42edd354d4804b086967037d3f97d7dc7df4a2b1
VOncleyton Martins <martins.voncleyton@gmail.com>
2023-01-02 19:30:19 -0300

    refactor: create a describe to Company Entity tests

--- src/entities/company.spec.ts
+++ src/entities/company.spec.ts
@@ -2,26 +2,31 @@ import { Company } from "./company";
 import { CreateCompany, ICreateCompanyDTO } from "./create-company";
 
 const VALID_CNPJ = '14.245.016/0001-79'
-it('should throw an error when company\'s CNPJ is invalid', () => {
-  expect(() => {const company = new Company({
-    CNPJ: 'invalid_CNPJ',
-    name: 'any_name',
-    address: 'any_address',
-    phone: 'any_phone',
-    motorcycleLots: 1,
-    carLots: 1
-  })}).toThrow();
-});
 
-it('should throw an error when no parking lots of any type are provided', () => {
-  expect(() => {
-    const company = new Company({
-      CNPJ: VALID_CNPJ,
-      name: 'any_name',
-      address: 'any_address',
-      phone: 'any_phone',
-      motorcycleLots: 0,
-      carLots: 0
-    })
-  }).toThrow();
-});
+describe('Company Entity', () => {
+  it('should throw an error when company\'s CNPJ is invalid', () => {
+    expect(() => {
+      const company = new Company({
+        CNPJ: 'invalid_CNPJ',
+        name: 'any_name',
+        address: 'any_address',
+        phone: 'any_phone',
+        motorcycleLots: 1,
+        carLots: 1
+      })
+    }).toThrow();
+  });
+  
+  it('should throw an error when no parking lots of any type are provided', () => {
+    expect(() => {
+      const company = new Company({
+        CNPJ: VALID_CNPJ,
+        name: 'any_name',
+        address: 'any_address',
+        phone: 'any_phone',
+        motorcycleLots: 0,
+        carLots: 0
+      })
+    }).toThrow();
+  });
+})
